7f3163283b8a9e15908407e8a2d504636a6b4261
Cezar Fernando <cezarfbc@gmail.com>
2025-06-22 13:30:55 -0300

    feat: add container management endpoints for starting, pausing, deleting containers and images

--- src/main/java/czar/com/my/heroku/api/my_heroku_api/controllers/ContainerController.java
+++ src/main/java/czar/com/my/heroku/api/my_heroku_api/controllers/ContainerController.java
@@ -7,9 +7,7 @@ import czar.com.my.heroku.api.my_heroku_api.services.ContainerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,25 +18,65 @@ public class ContainerController {
     @Autowired
     private ContainerService containerService;
 
-    @GetMapping("list-running-containers")
+    @GetMapping("/list-running-containers")
     public ResponseEntity<List<ListRunningContainersResponseDto>> listRunningContainers() {
         List<ListRunningContainersResponseDto> response = this.containerService.listRunningContainers();
 
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("list-all-containers")
+    @GetMapping("/list-all-containers")
     public ResponseEntity<List<ListAllContainersResponseDto>> listAllContainers() {
         List<ListAllContainersResponseDto> response = this.containerService.listAllContainers();
 
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("list-all-images")
+    @PatchMapping("/start-container/{containerId}")
+    public ResponseEntity<Boolean> startContainer(@PathVariable("containerId") String containerId) {
+        /*
+         * must have implementation
+         * */
+        return ResponseEntity.ok(Boolean.TRUE);
+    }
+
+    @PatchMapping("/pause-container/{containerId}")
+    public ResponseEntity<Boolean> pauseContainer(@PathVariable("containerId") String containerId) {
+        /*
+         * must have implementation
+         * */
+        return ResponseEntity.ok(Boolean.TRUE);
+    }
+
+    @GetMapping("/get-log-one-container/{containerId}")
+    public ResponseEntity<Boolean> getInfoOneContainer(@PathVariable("containerId") String containerId) {
+        /*
+        * change return type to have logs
+        * */
+        return ResponseEntity.ok(Boolean.TRUE);
+    }
+
+    @DeleteMapping("/delete-container/{containerId}")
+    public ResponseEntity<Boolean> deleteContainer(@PathVariable("containerId") String containerId) {
+        /*
+         * must have implementation
+         * */
+        return ResponseEntity.ok(Boolean.TRUE);
+    }
+
+    @GetMapping("/list-all-images")
     public ResponseEntity<List<ListImagesResponseDto>> listAllImages() {
         List<ListImagesResponseDto> response = this.containerService.listAllImages();
 
         return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/delete-image/{imageId}")
+    public ResponseEntity<Boolean> deleteImage(@PathVariable("imageId") String imageId) {
+        /*
+        * must have implementation
+        * */
+        return ResponseEntity.ok(Boolean.TRUE);
     }
 
 }
